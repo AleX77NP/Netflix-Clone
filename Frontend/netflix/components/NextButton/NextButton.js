29aaa638a78f629bc9b050538471dfb978e1943f
Aleksandar Milanovic <milanovicaleX77@gmail.com>
2021-05-12 22:59:56 +0200

    prop types

--- Frontend/netflix/components/NextButton/NextButton.js
+++ Frontend/netflix/components/NextButton/NextButton.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import styles from './NextButton.module.css'
+import PropTypes from 'prop-types';
 
 const NextButton = ({ onPress, isDisabled }) => {
     return (
@@ -7,6 +8,11 @@ const NextButton = ({ onPress, isDisabled }) => {
             <button onClick={onPress} disabled={isDisabled} className={styles.button}>Continue</button>
         </div>
     )
+}
+
+NextButton.propTypes = {
+    onPress: PropTypes.func,
+    isDisabled: PropTypes.bool
 }
 
 export default NextButton
