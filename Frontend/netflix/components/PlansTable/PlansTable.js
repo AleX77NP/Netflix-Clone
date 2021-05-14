775e67e87e1e35c2f97ffafed4c24fcb186cc861
Aleksandar Milanovic <milanovicaleX77@gmail.com>
2021-05-14 12:43:10 +0200

    plans done

--- Frontend/netflix/components/PlansTable/PlansTable.js
+++ Frontend/netflix/components/PlansTable/PlansTable.js
@@ -12,27 +12,27 @@ const PlansTable = () => {
                 <tbody>
                     <tr>
                         <td className={styles.th_cell}>Monthly price</td>
-                        <td className={styles.td_cell}>EUR{plans[0].monthlyPrice}</td>
-                        <td className={styles.td_cell}>EUR{plans[1].monthlyPrice}</td>
-                        <td className={styles.td_cell}>EUR{plans[2].monthlyPrice}</td>
+                        <td className={state.plan === 1 ? styles.td_cell_active : styles.td_cell}>EUR{plans[0].monthlyPrice}</td>
+                        <td className={state.plan === 2 ? styles.td_cell_active : styles.td_cell}>EUR{plans[1].monthlyPrice}</td>
+                        <td className={state.plan === 3 ? styles.td_cell_active : styles.td_cell}>EUR{plans[2].monthlyPrice}</td>
                     </tr>
                     <tr>
                         <td className={styles.th_cell}>Video quality</td>
-                        <td className={styles.td_cell}>{plans[0].videoQuality}</td>
-                        <td className={styles.td_cell}>{plans[1].videoQuality}</td>
-                        <td className={styles.td_cell}>{plans[2].videoQuality}</td>
+                        <td className={state.plan === 1 ? styles.td_cell_active : styles.td_cell}>{plans[0].videoQuality}</td>
+                        <td className={state.plan === 2 ? styles.td_cell_active : styles.td_cell}>{plans[1].videoQuality}</td>
+                        <td className={state.plan === 3 ? styles.td_cell_active : styles.td_cell}>{plans[2].videoQuality}</td>
                     </tr>
                     <tr>
                         <td className={styles.th_cell}>Resolution</td>
-                        <td className={styles.td_cell}>{plans[0].resolution}</td>
-                        <td className={styles.td_cell}>{plans[1].resolution}</td>
-                        <td className={styles.td_cell}>{plans[2].resolution}</td>
+                        <td className={state.plan === 1 ? styles.td_cell_active : styles.td_cell}>{plans[0].resolution}</td>
+                        <td className={state.plan === 2 ? styles.td_cell_active : styles.td_cell}>{plans[1].resolution}</td>
+                        <td className={state.plan === 3 ? styles.td_cell_active : styles.td_cell}>{plans[2].resolution}</td>
                     </tr>
                     <tr id={styles.bottom_tr}>
                         <td className={styles.th_cell}>Watch on your TV, computer, mobile phone and tablet</td>
-                        <td className={styles.td_cell}>&#10003;</td>
-                        <td className={styles.td_cell}>&#10003;</td>
-                        <td className={styles.td_cell}>&#10003;</td>
+                        <td className={state.plan === 1 ? styles.td_cell_active : styles.td_cell}>&#10003;</td>
+                        <td className={state.plan === 2 ? styles.td_cell_active : styles.td_cell}>&#10003;</td>
+                        <td className={state.plan === 3 ? styles.td_cell_active : styles.td_cell}>&#10003;</td>
                     </tr>
                 </tbody>
             </table>
