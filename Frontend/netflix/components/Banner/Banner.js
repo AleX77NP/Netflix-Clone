572c1e5943525e8aac84ec6a8e6dc103424bde52
Aleksandar Milanovic <milanovicaleX77@gmail.com>
2021-05-28 16:29:25 +0200

    banner mobile

--- Frontend/netflix/components/Banner/Banner.js
+++ Frontend/netflix/components/Banner/Banner.js
@@ -6,18 +6,22 @@ import styles from './Banner.module.css'
 const Banner = () => {
 
     const {bannerMovie, error} = useMoviesObservable(baseURL, 'originals')
-    console.log(bannerMovie)
+
+    function truncate(str, n) {
+        return str?.length > n ? str.substr(0, n - 1) + "..." : str;
+      }
 
     return (
-        <header className={styles.wrapper}>
-            <div className={styles.container} style={{backgroundImage: `url("https://image.tmdb.org/t/p/original${bannerMovie?.backdrop_path}")`, backgroundSize: "cover", backdropPosition: "center center"}}>
+        <header className={styles.wrapper} style={{backgroundImage: `url("https://image.tmdb.org/t/p/original${bannerMovie?.backdrop_path}")`, backgroundSize: "cover",backdropPosition: "center center"}}>
+            <div className={styles.container}>
                 <h1 className={styles.title}>{bannerMovie?.title || bannerMovie?.original_name || bannerMovie?.name}</h1>
                 <div className={styles.buttons}>
                     <button className={styles.banner_btn}>Play</button>
                     <button className={styles.banner_btn}>More Info</button>
                 </div>
-                <h1 className={styles.overview}>{bannerMovie?.overview}</h1>
+                <h1 className={styles.overview}>{truncate(bannerMovie?.overview)}</h1>
             </div>
+            <div className={styles.fade} />
         </header>
     )
 }
