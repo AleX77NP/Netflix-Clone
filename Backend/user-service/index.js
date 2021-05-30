010596650931a6bc96d6f64b272c5884b3e36c0a
Aleksandar Milanovic <milanovicaleX77@gmail.com>
2021-05-30 16:10:38 +0200

    user service registered with eureka

--- Backend/user-service/index.js
+++ Backend/user-service/index.js
@@ -2,6 +2,36 @@
 const fastify = require('fastify')({ logger: true })
 const mongoose = require('mongoose')
 
+const Eureka = require('eureka-js-client').Eureka;
+
+const eureka = new Eureka({
+  instance: {
+    app: 'USER-SERVICE',
+    hostName: 'localhost',
+    ipAddr: '127.0.0.1',
+    statusPageUrl: 'http://localhost:9003',
+    port: {
+      '$': 9003,
+      '@enabled': 'true',
+    },
+    vipAddress: 'user-service',
+    dataCenterInfo: {
+      '@class': 'com.netflix.appinfo.InstanceInfo$DefaultDataCenterInfo',
+      name: 'MyOwn',
+    }
+  },
+  eureka: {
+    host: 'localhost',
+    port: 8761,
+    servicePath: '/eureka/apps/'
+  }
+});
+eureka.logger.level('debug');
+eureka.start(function(error){
+  console.log(error || 'complete');
+});
+
+
 require('dotenv').config()
 
 fastify.register(require('fastify-cookie'), {
@@ -26,7 +56,7 @@ fastify.register(require('./routes/user-routes'))
 // Run the server
 const start = async () => {
   try {
-    await fastify.listen(9000)
+    await fastify.listen(9003)
   } catch (err) {
     fastify.log.error(err)
     process.exit(1)
